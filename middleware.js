fc61679e6ce97d88ae02feb59970c4953e30b720
misung <voielactee7@naver.com>
2024-04-11 14:13:08 +0900

    상태저장 체이닝

---
+++ middleware.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const middle1 = require("./my_modules/middle1");
+
+// 미들웨어 등록 use:모든 method(get, post, put, delete등등)에 대해 처리
+// arg로 경로를 지정하지 않으면, 모든 경로에 대해 처리
+app.use(middle1);
+
+app.get("/", (req, res, next) => {
+  // 애플리케이션 수준의 상태 저장
+  req.app.locals.message = "Hello World";
+  next();
+});
+
+app.get("/", (req, res) => {
+  // 응답 수준의 상태 저장
+  res.locals.additionalMessage = "swag";
+
+  // 저장된 상태 값 사용
+  const message = req.app.locals.message + res.locals.additionalMessage;
+  console.log(message);
+  res.send(message);
+});
+
+app.listen(3000);
