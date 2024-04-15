e43a7e8d6bd343f274cfcf72335ab49ac9d83f35
misung <voielactee7@naver.com>
2024-04-15 11:06:52 +0900

    ejs

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ const path = require("path"); // node에서 기본으로 제공하는 패키지
 const app = express();
 const port = 3000;
 
+app.set("views", "templates"); // 뷰 디렉토리를 'templates'로 설정
+app.set("view engine", "ejs"); // 뷰 엔진을 EJS로 설정
+
 app.use("/HTML", express.static(path.join(__dirname, "HTML")));
 
 // get method 요청에 대한 라우팅
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  const data = {
+    name: "JEONG MIN",
+    age: "20",
+    skills: ["JS", "Node.js", "React"],
+  };
+  res.render("index", data);
 });
 
 // 예) http://localhost:3000/test/clover?lang=ko
--- middleware.js
+++ middleware.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const middle1 = require("./My_Modules/middle1");
+const morgan = require("morgan");
 
 // 미들웨어 등록 use:모든 method(get, post, put, delete등등)에 대해 처리
 // arg로 경로를 지정하지 않으면, 모든 경로에 대해 처리
+app.use(morgan("tiny"));
 app.use(middle1);
 
 app.get("/", (req, res, next) => {
